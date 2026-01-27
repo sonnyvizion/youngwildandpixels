768f5f2eb76f23621f9ff99b0248dbac619567ef
Sonny Marcin <sonnvizion@gmail.com>
2026-01-27 14:20:07 +0100

    Add Vite base scripts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/youngwildandpixels/',
+  base: process.env.VITE_BASE || '/',
   server: {
     port: 5173,
     open: true
